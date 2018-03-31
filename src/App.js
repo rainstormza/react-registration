b208776931ed4de9c0b5188f0ced075995a548b4
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-03-31 13:59:06 +0700

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App Site">
+        <div className="Site-content">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <div className="main">
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
